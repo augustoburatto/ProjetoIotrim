d6382e6bda6885f4966c5051344f90ac51bead50
augustoburatto <54562178+augustoburatto@users.noreply.github.com>
2021-08-03 23:41:35 -0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:iotrim/pages/loginPage.dart';
+
+void main () => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'IOTrim',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.deepOrangeAccent,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
+
